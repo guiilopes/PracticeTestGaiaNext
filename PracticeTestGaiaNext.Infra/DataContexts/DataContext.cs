a03603e5b00dfbc397550f3f73eab180d66cf3ac
Guilherme Lopes <guii-lopes@hotmail.com>
2020-11-27 13:39:09 -0300

    feat(project): upload new project

---
+++ PracticeTestGaiaNext.Infra/DataContexts/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticeTestGaiaNext.Infra.DataContexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
